d50fcbdffa6a722d3ad9cef87e893549c94e9771
venkatasravan <74409661+venkatasravan@users.noreply.github.com>
2021-06-29 13:00:32 +0100

    add react code

---
+++ src/yourdata.js
@@ -0,0 +1,136 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+import linkedinIcon from "./images/linkedin.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Nalajarla Venkata Sravan Kumar",
+  headerTagline: [
+    //Line 1 For Header
+    "Application Developer,",
+    //Line 2 For Header
+    "Proficient in Automation Testing,",
+    //Line 3 For Header
+    "and Automating Infrastructure",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Currently pursuing Masters in Advanced Computer Science at Northumbria University. Seeking a software developer job opportunity to utilize my skills in the industry and to gain experience.",
+
+  //Contact Email
+  contactEmail: "sravan8801012643@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Tata Consultancy Services ", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      imageSrc: "https://www.tass.gov.uk/wp-content/uploads/2018/06/NU_logo_black.jpg",
+
+    },
+    {
+      title: "Northumbria University", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  aboutParaTwo:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutParaThree:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: cssIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: jsIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: reactIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: designIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's work together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/venkata-sravan" },
+    {
+      img: linkedinIcon,
+      url: "https://www.linkedin.com/in/venkata-sravan-nalajarla-559a85128/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
